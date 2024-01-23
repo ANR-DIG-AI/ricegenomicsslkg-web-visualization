d7eaae5696d1c3f47b8369b91e3095ad291f56b1
Franck Michel <frmichel@users.noreply.github.com>
2024-01-23 10:40:45 +0100

    Minor refactoring

--- src/Component/search/SearchForm.js
+++ src/Component/search/SearchForm.js
@@ -25,8 +25,6 @@ import {suggestionsMock} from './suggestions.mock';
  * @constructor
  */
 function SearchForm() {
-    // Term typed in the input field
-    const [input, setInput] = useState('');
 
     // Suggestions for autocompletion.
     // Each suggestion should be an object like {entityLabel: "...", entityUri: "...", entityPrefLabel: "..."}
@@ -35,17 +33,18 @@ function SearchForm() {
     // Search entities already selected
     const [searchEntities, setSearchEntities] = useState([]);
 
-    // Status of the search button (loading search results)
-    const [isLoading, setLoading] = useState(false);
+    // Term typed in the input field
+    const [input, setInput] = useState('');
 
+    // Status of the loading spinner (search for exact match)
+    const [isLoadingExactMatch, setLoadingExactMatch] = useState(false);
     // Results returned by the last search with exact match
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchResultsExactMatch, setSearchResultsExactMatch] = useState([]);
 
+    // Status of the loading spinner (search for sub-concepts)
+    const [isLoadingSubConcepts, setIsLoadingSubConcepts] = useState(false);
     // Results returned by the last search including sub-concepts
     const [searchResultsSubConcept, setSearchResultsSubConcept] = useState([]);
-
-    // Status of the search button (loading search results for sub-concepts)
-    const [isLoadingSubConcepts, setIsLoadingSubConcepts] = useState(false);
 
 
     /**
@@ -159,7 +158,7 @@ function SearchForm() {
      * Click start button action
      */
     const startSearch = () => {
-        setLoading(true);
+        setLoadingExactMatch(true);
         setIsLoadingSubConcepts(true);
     }
 
@@ -169,35 +168,35 @@ function SearchForm() {
      */
     useEffect(() => {
         setSearchResultsSubConcept([]);
-        if (isLoading) {
+        if (isLoadingExactMatch) {
             if (searchEntities.length === 0) {
                 if (process.env.REACT_APP_LOG === "on") {
                     console.log("------------------------- No search entity was selected, not invoking search service.");
                 }
-                setLoading(false);
-                setSearchResults([]);
+                setLoadingExactMatch(false);
+                setSearchResultsExactMatch([]);
             } else {
                 let query = process.env.REACT_APP_BACKEND_URL + "/searchDocuments/?uri=" + searchEntities.map(_s => _s.entityUri).join(',');
                 if (process.env.REACT_APP_LOG === "on") {
                     console.log("Will submit backend query: " + query);
                 }
                 axios(query).then(response => {
-                    setLoading(false);
+                    setLoadingExactMatch(false);
                     if (isEmptyResponse(query, response)) {
-                        setSearchResults([]);
+                        setSearchResultsExactMatch([]);
                     } else {
                         let _results = response.data.result;
                         if (process.env.REACT_APP_LOG === "on") {
                             console.log("------------------------- Retrieved " + _results.length + " search results.");
                             //_results.forEach(e => console.log(e));
                         }
-                        setSearchResults(_results);
+                        setSearchResultsExactMatch(_results);
                     }
                 })
             }
         }
         //eslint-disable-next-line
-    }, [isLoading]);
+    }, [isLoadingExactMatch]);
 
 
     /**
@@ -222,13 +221,13 @@ function SearchForm() {
 
                 // Filter the results to keep only those documents that were not in the first set of results (with exact match)
                 let additionalResults = _results.filter((_a) =>
-                    !searchResults.find((_r) => _r.document === _a.document)
+                    !searchResultsExactMatch.find((_r) => _r.document === _a.document)
                 );
                 setSearchResultsSubConcept(additionalResults);
             }
         })
         //eslint-disable-next-line
-    }, [searchResults]);
+    }, [searchResultsExactMatch]);
 
 
     return (
@@ -236,19 +235,6 @@ function SearchForm() {
             <div className="component">
                 <h1 className="">Search articles by named entities</h1>
                 <div className="multiple-inputs-container">
-
-                    { /* List of the search entities that have already been selected */}
-                    <div className="entity-list">
-                        {searchEntities.map((entity, index) => (
-                            <SearchEntity key={index} id={index}
-                                          entityUri={entity.entityUri}
-                                          entityLabel={entity.entityLabel}
-                                          entityPrefLabel={entity.entityPrefLabel}
-                                          entityType={entity.entityType}
-                                          handleRemove={handleRemoveEntity}
-                            />
-                        ))}
-                    </div>
 
                     <Form>
                         { /* Input field and search button */}
@@ -264,9 +250,9 @@ function SearchForm() {
                             </Col>
                             <Col xs={2}>
                                 <Button id="search-button" className="search-button" variant="secondary"
-                                        disabled={isLoading}
-                                        onClick={!isLoading ? () => startSearch() : null}>
-                                    {isLoading ? 'Searching...' : 'Search'}
+                                        disabled={isLoadingExactMatch}
+                                        onClick={!isLoadingExactMatch ? () => startSearch() : null}>
+                                    {isLoadingExactMatch ? 'Searching...' : 'Search'}
                                 </Button>
                             </Col>
                         </Row>
@@ -296,6 +282,19 @@ function SearchForm() {
                         </ListGroup>
                     </Form>
 
+                    { /* List of the search entities that have already been selected */}
+                    <div className="entity-list">
+                        {searchEntities.map((entity, index) => (
+                            <SearchEntity key={index} id={index}
+                                          entityUri={entity.entityUri}
+                                          entityLabel={entity.entityLabel}
+                                          entityPrefLabel={entity.entityPrefLabel}
+                                          entityType={entity.entityType}
+                                          handleRemove={handleRemoveEntity}
+                            />
+                        ))}
+                    </div>
+
                 </div>
             </div>
 
@@ -308,9 +307,9 @@ function SearchForm() {
                         Results for only the selected named entities
                     </div>
                     <div className="loading-spinner">
-                        <RotatingLines visible={isLoading} height="50" width="50"/>
+                        <RotatingLines visible={isLoadingExactMatch} height="50" width="50"/>
                     </div>
-                    <SearchResultsList searchResults={searchResults}/>
+                    <SearchResultsList searchResults={searchResultsExactMatch}/>
                 </div>
             }
 
